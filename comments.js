67070b9a2b29344bde00fdda812fa6a22b20e282
Oleksandr Nikiforov <nikiforov.aleksandr@gmail.com>
2024-10-14 22:56:57 +0000

    Create express app

--- comments.js
+++ comments.js
@@ -1,42 +1,12 @@
-// create web server
-
+// Create web server
+// Create express app
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
-
-app.use(bodyParser.json());
-
+// Create a route to handle GET requests to the URL /comments
 app.get('/comments', (req, res) => {
-    fs.readFile('./comments.json', (err, data) => {
-        if (err) {
-            res.status(500).send('Internal Server Error');
-        } else {
-            const comments = JSON.parse(data);
-            res.status(200).send(comments);
-        }
-    });
+  res.send('This is the comments route');
 });
-
-app.post('/comments', (req, res) => {
-    fs.readFile('./comments.json', (err, data) => {
-        if (err) {
-            res.status(500).send('Internal Server Error');
-        } else {
-            const comments = JSON.parse(data);
-            const newComment = req.body;
-            comments.push(newComment);
-            fs.writeFile('./comments.json', JSON.stringify(comments), (err) => {
-                if (err) {
-                    res.status(500).send('Internal Server Error');
-                } else {
-                    res.status(201).send('Created');
-                }
-            });
-        }
-    });
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+// Start the server on port 4001
+app.listen(4001, () => {
+  console.log('Your app is listening on port 4001');
 });
