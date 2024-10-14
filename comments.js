dd29d6ed8d8abdee41ffc2298934b5e545e7e859
Oleksandr Nikiforov (Akvelon Inc) <v-onikiforov@microsoft.com>
2024-10-14 15:46:54 -0700

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,42 +1,43 @@
 // create web server
 
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
+console.log('comments.js webserver is running');
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
+// const fs = require('fs');
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.get('/comments', (req, res) => {
-    fs.readFile('./comments.json', (err, data) => {
-        if (err) {
-            res.status(500).send('Internal Server Error');
-        } else {
-            const comments = JSON.parse(data);
-            res.status(200).send(comments);
-        }
-    });
-});
+// app.get('/comments', (req, res) => {
+//     fs.readFile('./comments.json', (err, data) => {
+//         if (err) {
+//             res.status(500).send('Internal Server Error');
+//         } else {
+//             const comments = JSON.parse(data);
+//             res.status(200).send(comments);
+//         }
+//     });
+// });
 
-app.post('/comments', (req, res) => {
-    fs.readFile('./comments.json', (err, data) => {
-        if (err) {
-            res.status(500).send('Internal Server Error');
-        } else {
-            const comments = JSON.parse(data);
-            const newComment = req.body;
-            comments.push(newComment);
-            fs.writeFile('./comments.json', JSON.stringify(comments), (err) => {
-                if (err) {
-                    res.status(500).send('Internal Server Error');
-                } else {
-                    res.status(201).send('Created');
-                }
-            });
-        }
-    });
-});
+// app.post('/comments', (req, res) => {
+//     fs.readFile('./comments.json', (err, data) => {
+//         if (err) {
+//             res.status(500).send('Internal Server Error');
+//         } else {
+//             const comments = JSON.parse(data);
+//             const newComment = req.body;
+//             comments.push(newComment);
+//             fs.writeFile('./comments.json', JSON.stringify(comments), (err) => {
+//                 if (err) {
+//                     res.status(500).send('Internal Server Error');
+//                 } else {
+//                     res.status(201).send('Created');
+//                 }
+//             });
+//         }
+//     });
+// });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+// app.listen(3000, () => {
+//     console.log('Server is running on http://localhost:3000');
+// });
